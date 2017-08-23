ed1495579d89a0ea66acf74c3f603a9c31dfa2e5
Jake LaFond <jakelafond@gmail.com>
2017-08-23 15:42:08 -0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace css_viewmodels_and_.net.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
